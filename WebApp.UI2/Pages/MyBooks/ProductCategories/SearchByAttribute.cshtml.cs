9d9593b9d96672027cd54a679d147b3288293dc4
deepakkhemchandani27 <deepakkhemchandani27@gmail.com>
2021-12-09 11:15:01 +0530

    started working on search product varient

---
+++ WebApp.UI2/Pages/MyBooks/ProductCategories/SearchByAttribute.cshtml.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WebApp.UI2.Helpers;
+
+namespace WebApp.UI2.Pages.MyBooks.ProductCategories
+{
+    public class SearchByAttributeModel : PageModel
+    {
+        public string ApiUrl { get; }
+        private readonly ICookieHelper _cookieHelper;
+        public SearchByAttributeModel(ICookieHelper cookieHelper)
+        {
+            ApiUrl = ApiUrls.Rootlocal;
+            _cookieHelper = cookieHelper;
+        }
+        [BindProperty] public InputModel Input { get; set; }
+        public class InputModel
+        {
+            public string Name { get; set; }
+            public string CompanyId { get; set; }
+            public string CategoryId { get; set; }
+            public SelectList ProductCategorySelectList { get; set; }
+        }
+        public class ProductCategorySelectViewModel
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public string Type { get; set; }
+        }
+        [BindProperty]
+        public IEnumerable<ProductCategorySelectViewModel> ProductCategories { get; set; }
+
+        public async Task<IEnumerable<ProductCategorySelectViewModel>> GetCategories()
+        {
+            var cmpid = _cookieHelper.Get("cmpCookee");
+
+            if (string.IsNullOrEmpty(cmpid) && string.IsNullOrEmpty(cmpid))
+            {
+                return null;
+            }
+            using var client = new HttpClient();
+            var getProductsUri = new Uri(ApiUrls.ProductCategories.GetProductCategories + "?PageNumber=1&PageSize=10&cmpId=" + cmpid);
+
+            var userAccessToken = User.Claims.Where(x => x.Type == "AcessToken").FirstOrDefault().Value;
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userAccessToken);
+
+            var getUserInfo = await client.GetAsync(getProductsUri);
+
+            string resultuerinfo = getUserInfo.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            if (resultuerinfo != null)
+            {
+                var data = JsonConvert.DeserializeObject<IEnumerable<ProductCategorySelectViewModel>>(resultuerinfo);
+                ProductCategories = data;
+            }
+
+            return ProductCategories;
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            var cmpid = _cookieHelper.Get("cmpCookee");
+
+            if (string.IsNullOrEmpty(cmpid) && string.IsNullOrEmpty(cmpid))
+            {
+                return RedirectToPage("/");
+            }
+            InputModel inputModel = new InputModel();
+            inputModel.CompanyId = cmpid;
+            Input = inputModel;
+            var data = await GetCategories();
+            inputModel.ProductCategorySelectList = new SelectList(data, "Id", "Name");
+            return Page();
+
+        }
+    }
+}
