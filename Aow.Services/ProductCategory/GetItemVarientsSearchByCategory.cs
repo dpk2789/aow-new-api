8dd8561ab90b2a5d39d952159d6b999cce152850
deepakkhemchandani27 <deepakkhemchandani27@gmail.com>
2021-12-11 10:04:42 +0530

    bug fixes serch by att

---
+++ Aow.Services/ProductCategory/GetItemVarientsSearchByCategory.cs
@@ -0,0 +1,62 @@
+﻿using Aow.Infrastructure.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aow.Services.ProductCategory
+{
+    public class GetItemVarientsSearchByCategory
+    {
+        private IRepositoryWrapper _repoWrapper;
+        public GetItemVarientsSearchByCategory(IRepositoryWrapper repoWrapper)
+        {
+            _repoWrapper = repoWrapper;
+        }
+        public class GetProductCategoryResponse
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public Guid CompanyId { get; set; }
+            public Guid? ParentCategoryId { get; set; }
+            public IList<AttributesResponse> AttributesViewModel { get; set; }
+            public IList<ProductVariantsResponse> ProductVariantsViewModel { get; set; }
+        }
+        public class ProductVariantsResponse
+        {
+            public Guid Id { get; set; }         
+            public string Name { get; set; }  
+            public IList<AttributesResponse> AttributesViewModels { get; set; }
+
+        }
+        public class AttributesResponse
+        {
+            public Guid? Id { get; set; }
+            public string Name { get; set; }
+            public IEnumerable<AttributesOptionsViewModel> AttributesOptionsViewModels { get; set; }
+            public bool IsChecked { get; set; }
+        }
+
+        public class AttributesOptionsViewModel
+        {
+            public Guid? Id { get; set; }
+            public string Name { get; set; }
+            public bool IsChecked { get; set; }
+        }
+        public GetProductCategoryResponse Do(Guid id)
+        {
+            var retriveCategory = _repoWrapper.ProductCategoryRepo.GetProductCategory(id);          
+            var getCompanyResponse = new GetProductCategoryResponse
+            {
+                Id = retriveCategory.Id,
+                Name = retriveCategory.Name,
+
+            };
+            foreach (var category in retriveCategory.ProductAttributes)
+            {
+               
+            }           
+
+            return getCompanyResponse;
+        }
+    }
+}
--- WebApp.UI2/Pages/MyBooks/ProductCategories/SearchResultByAttribute.cshtml.cs
+++ WebApp.UI2/Pages/MyBooks/ProductCategories/SearchResultByAttribute.cshtml.cs
@@ -1,12 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WebApp.UI2.Helpers;
 
 namespace WebApp.UI2.Pages.MyBooks.ProductCategories
 {
     public class SearchResultByAttributeModel : PageModel
     {
-        public void OnGet()
+        public string ApiUrl { get; }
+        private readonly ICookieHelper _cookieHelper;
+        public SearchResultByAttributeModel(ICookieHelper cookieHelper)
         {
+            ApiUrl = ApiUrls.Rootlocal;
+            _cookieHelper = cookieHelper;
+        }
+        [BindProperty] public InputModel Input { get; set; }
+        public class InputModel
+        {
+            public string Name { get; set; }
+            public string CompanyId { get; set; }
+            public string CategoryId { get; set; }           
+        }
+      
+        public async Task<IActionResult> OnGet(Guid id)
+        {
+            using var client = new HttpClient();
+            var updateProductsUri = new Uri(ApiUrls.ProductCategories.GetProductCategory + "?id=" + id);
+
+            var userAccessToken = User.Claims.FirstOrDefault(x => x.Type == "AcessToken").Value;
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userAccessToken);
+
+            var postTask = await client.GetAsync(updateProductsUri);
+            var result = postTask.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            if (result != null)
+            {
+                var data = JsonConvert.DeserializeObject<InputModel>(result);
+                Input = data;
+            }           
+            return Page();            
         }
     }
 }
